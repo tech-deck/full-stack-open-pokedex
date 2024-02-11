6228df6107da26d48322451430f0f6108366c1b5
tech-deck <elliottbotha@gmail.com>
2024-02-12 08:20:34 +1100

    11.2 - build should fail

--- app.js
+++ app.js
@@ -5,11 +5,19 @@ const app = express()
 const PORT = process.env.PORT || 5000
 
 app.get('/version', (req, res) => {
-  res.send('1') // change this string to ensure a new version deployed
+  res.send('2') // change this string to ensure a new version deployed
 })
+
+app.get('/health', (req, res) => {
+  // eslint-disable-next-line no-constant-condition
+  if (true) throw('error...  ')
+  res.send('ok')
+})
+
 
 app.use(express.static('dist'))
 
 app.listen(PORT, () => {
+
   console.log(`server started on port ${PORT}`) // eslint-disable-line no-console
 })
